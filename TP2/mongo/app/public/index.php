df3475445075cfda2c84fb741474be1fa2675b1b
U.N.Owen <lclerc561@gmail.com>
2025-12-28 15:12:48 +0100

    début tp2

---
+++ TP2/mongo/app/public/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: canals5
+ * Date: 28/10/2019
+ * Time: 16:16
+ */
+
+use MongoDB\Client;
+
+require_once __DIR__ . "/../src/vendor/autoload.php";
+
+$c = new Client("mongodb://mongo");
+echo "connected to mongo <br>";
+
